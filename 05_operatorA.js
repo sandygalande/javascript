17f6c98f99e64468f28bf9cc16e6e3f8923b9041
sandygalande <sandipgalande0076@gmail.com>
2024-01-01 11:53:12 +0530

    update operator
    update operator

--- 05_operatorA.js
+++ 05_operatorA.js
@@ -1,24 +1,24 @@
 
 
-console.log("-------------------Step 1----------------------")
+console.log("----------------------Step 1-------------------------")
 function squareOfWordLength(){
-    var givenString=`"JavaScript"`
-    console.log(`1.1) Given String is= ${givenString}`)
+    var givenString= `"JavaScript"`;
+    console.log(`1.1) Given String is= ${givenString}`);
     var totalChar= givenString.length;
     console.log(`     Length of string is= ${totalChar}`);
     var square =givenString.length * givenString.length;
     console.log(`     Square of string length is= ${square}`);
 
-    var givenString=`"Google Chrome"`
-    console.log(`1.2) Given String is= ${givenString}`)
+    var givenString= `"Google Chrome"`;
+    console.log(`1.2) Given String is= ${givenString}`);
     var totalChar= givenString.length;
     console.log(`     Length of String is= ${totalChar}`);
     var square =givenString.length * givenString.length;
     console.log(`     Square of string length is= ${square}`);
 
     
-    var givenString=`"Developer Smart"`
-    console.log(`1.3) Given String is= ${givenString}`)
+    var givenString= `"Developer Smart"`;
+    console.log(`1.3) Given String is= ${givenString}`);
     var totalChar= givenString.length;
     console.log(`     Length of String is= ${totalChar}`);
     var square =givenString.length * givenString.length;
@@ -26,9 +26,9 @@ function squareOfWordLength(){
 }
 squareOfWordLength()
 
-console.log("-------------------Step 2----------------------")
+console.log("----------------------Step 2-------------------------")
 function string(){
-    var givenString=`"I am Angular Developer"`
+    var givenString= `"I am Angular Developer"`
     console.log(`2) Given String is=${givenString}`);
 
     var totalChar= givenString.length;
@@ -37,12 +37,12 @@ function string(){
     var result = givenString.split(" ");
     console.log(`   Total words in the string is= ${result.length} `);
 
-    var result= totalChar / result.length;
-    console.log(`2.1) Division of string length and total number of words is= ${result}`);
+    var div= totalChar / result.length;
+    console.log(`2.1) Division of string length and total number of words is= ${div}`);
 
-    var mul = totalChar * result;
-    console.log(`2.2) multiplication of string length and total words available in string is = ${mul}`)
-
+    var result= totalChar * result.length;
+    console.log(`2.2) Multiplication of string length and total number of words is= ${result}`);
 }
 string();
 
+    
