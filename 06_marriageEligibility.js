01fb4c9159cb8437ecd185d970dce9a92255f538
sandygalande <sandipgalande0076@gmail.com>
2024-01-05 00:26:34 +0530

    Update marriage eligibility
    Update marriage eligibility

--- 06_marriageEligibility.js
+++ 06_marriageEligibility.js
@@ -1,16 +1,22 @@
-
-console.log("<==========================Assignment 2===========================>");
-var marriageEligibility = function (gender,age){
-    if (age>=21 && age>=18) {
-        console.log(`Age ${age} is eligible for marriage.`);    
-    } else {
-        console.log(`Age ${age} is not eligible for marriage.`); 
-
-    }
-}
-marriageEligibility("Male",17);
-marriageEligibility("Male",25);
-marriageEligibility("Female",28);
-marriageEligibility("Female",16);
-marriageEligibility("Other",35);
-marriageEligibility("Other",41);
+console.log("<=============================Assignment 2==============================>");
+var marriageEligibility = function (gender, age) {
+  if (gender === "Male" && age >= 21) {
+    console.log(
+      `Gender is ${gender} & Age is ${age}, then you are eligible for marriage.`
+    );
+  } else if (gender === "Female" && age >= 18) {
+    console.log(
+      `Gender is ${gender} & Age is ${age}, then you are eligible for marriage.`
+    );
+  } else {
+    console.log(
+      `Gender is ${gender} & Age is ${age}, then you are not eligible for marriage.`
+    );
+  }
+};
+marriageEligibility("Male", 17);
+marriageEligibility("Male", 25);
+marriageEligibility("Female", 28);
+marriageEligibility("Female", 16);
+marriageEligibility("Other", 35);
+marriageEligibility("Other", 41);
